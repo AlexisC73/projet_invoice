fe56cb08d68b7397e47b0637374e698eefbd3dfc
AlexisC73 <alexis.comte73@icloud.com>
2023-01-05 16:34:43 +0100

    https options

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import * as cors from 'cors'
 import { AppDataSource } from './data-source'
 import * as cookieParser from 'cookie-parser'
 
+import { createServer } from 'https'
+
+import * as fs from 'fs'
+
 import * as session from 'express-session'
 import * as passport from 'passport'
 
@@ -19,6 +23,11 @@ AppDataSource.initialize()
   .catch(error => console.log(error))
 
 const PORT = process.env.PORT || 5500
+
+const option = {
+  key: fs.readFileSync('/etc/letsencrypt/live/alexis-comte.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/alexis-comte.com/fullchain.pem'),
+}
 
 const corsOrigin = {
   origin: ['http://localhost:3000', 'https://invoice.alexis-comte.com'],
@@ -43,6 +52,10 @@ app.use(passport.authenticate('session'))
 app.use('/invoice', invoiceRoute)
 app.use('/user', userRoute)
 
-app.listen(PORT, () => {
-  console.log('Server started on port =>', PORT)
-})
+process.env.NODE_ENV === 'production'
+  ? createServer(option, app).listen(PORT, () => {
+      console.log('Server started on port =>', PORT)
+    })
+  : app.listen(PORT, () => {
+      console.log('Server started on port =>', PORT)
+    })
