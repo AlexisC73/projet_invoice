9d79bc1604ad674ca2def97fab3d1c19a2327dfd
Alexis Comte <alexis.comte73@icloud.com>
2023-04-13 12:26:45 +0200

    fix erreur docker du au lancement des tests d integration

--- src/joi/schema/user/index.ts
+++
@@ -1,7 +0,0 @@
-import * as Joi from 'joi'
-
-export const userSchema = Joi.object({
-  id: Joi.string().allow(null).empty('').optional(),
-  email: Joi.string().email().required().not().empty(),
-  password: Joi.string().required().not().empty(),
-})
